28ab0d1f306ce228e77bcf5e6c45ce04e6a7c5d5
dsemer <dimitris.t.semertzidis@gmail.com>
2026-06-17 08:03:19 +0300

    Update meteo.py

--- meteo.py
+++ meteo.py
@@ -1,7 +1,7 @@
-    import streamlit as st
-    import requests
-    import pandas as pd
-    from datetime import datetime, timedelta
+import streamlit as st
+import requests
+import pandas as pd
+from datetime import datetime, timedelta
 
     # -------------------
     # APP CONFIG
